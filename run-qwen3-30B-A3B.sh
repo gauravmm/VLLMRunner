db302923e50a980741cb0cbcafb7f45db9115d03
Gaurav Manek <gaurav@gauravmanek.com>
2026-03-14 04:14:08 +0000

    Adding various run configs.

--- run-qwen3-30B-A3B.sh
+++ run-qwen3-30B-A3B.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-uv run vllm serve cyankiwi/Qwen3.5-9B-AWQ-4bit \
+uv run vllm serve QuantTrio/Qwen3-VL-30B-A3B-Thinking-AWQ \
   --trust-remote-code \
-  --max-model-len 131072 \
+  --max-model-len 24000 \
   --gpu-memory-utilization 0.95 \
-  --served-model-name qwen3.5-9b-awq \
-  --kv-cache-dtype fp8 \
+  --served-model-name qwen3-vl-moe \
   --enable-auto-tool-choice \
   --tool-call-parser hermes \
   --reasoning-parser qwen3 \
