b90844553e0d1f092c1296a35d41a9f8eab0f7e5
Gaurav Manek <gaurav@gauravmanek.com>
2026-05-05 13:26:26 +0800

    There, works in parallel.

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 uv run vllm serve google/gemma-4-e4b-it \
   --trust-remote-code \
-  --max-model-len 16384 \
+  --max-model-len 24000 \
   --gpu-memory-utilization 0.90 \
   --served-model-name google/gemma-4-e4b-it \
   --kv-cache-dtype fp8 \
@@ -12,6 +12,7 @@ uv run vllm serve google/gemma-4-e4b-it \
   --reasoning-parser gemma4 \
   --enable-chunked-prefill \
   --enable-log-requests \
+  --enable-prefix-caching \
   --max-num-seqs 32 \
-  --max-num-batched-tokens 16384 \
-  --port 8191
+  --max-num-batched-tokens 24000 \
+  --port 8
