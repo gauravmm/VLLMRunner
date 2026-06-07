3e69a6211ad9684b512693e046211f2987dc8254
Gaurav Manek <gaurav@gauravmanek.com>
2026-06-07 13:50:42 +0800

    Updated Gemma 4 w4a16

--- bench.sh
+++ bench.sh
@@ -1,9 +1,13 @@
 #/bin/sh
 
 uv run vllm bench serve \
-  --model google/gemma-4-12B-it-qat-w4a16-ct \
+  --model google/gemma-4-12B-it \
   --port 8139 \
   --dataset-name sharegpt \
   --dataset-path ./ShareGPT_V3_unfiltered_cleaned_split.json \
   --num-prompts 500 \
-  --request-rate 4
+  --request-rate 4 \
+  --temperature 1.0 \
+  --sharegpt-output-len 512 \
+  --percentile-metrics ttft,tpot,itl,e2el \
+  --metric-percentiles 90,95,99
--- run.sh
+++ run.sh
@@ -5,8 +5,7 @@ uv run vllm serve google/gemma-4-12B-it-qat-w4a16-ct \
   --max-model-len 24000 \
   --gpu-memory-utilization 0.90 \
   --served-model-name google/gemma-4-12B-it \
-  --dtype bfloat16 \
-  --hf-overrides '{"vision_config": {"num_soft_tokens": 280}}' \
+  --hf-overrides '{"vision_config": {"num_soft_tokens": 1120}}' \
   --enable-auto-tool-choice \
   --tool-call-parser gemma4 \
   --reasoning-parser gemma4 \
@@ -15,5 +14,4 @@ uv run vllm serve google/gemma-4-12B-it-qat-w4a16-ct \
   --enable-prefix-caching \
   --max-num-seqs 16 \
   --max-num-batched-tokens 24000 \
-  --language-model-only \
   --port 8139
